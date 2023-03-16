1a39af7b43102c5b1371c0625f530cb639241b44
Siaw A. Nicholas <ayequillgh@gmail.com>
2023-03-16 15:51:39 +0000

    fix for betty styling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
 * main - print sizes of various data types
 *
 * Return: 0
@@ -12,7 +12,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
