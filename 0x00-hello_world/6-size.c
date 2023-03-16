33aa8cfc6a99ed5e7a6ce2bc636e0a606b5c180c
Siaw A. Nicholas <ayequillgh@gmail.com>
2023-03-16 15:45:24 +0000

    fix syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
 * main - print sizes of various data types
 *
@@ -18,6 +19,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(e));
-	
 	return (0);
 }
