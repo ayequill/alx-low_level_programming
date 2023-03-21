8e2e91274165b15c14e92e14d6d20fd297310be9
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-03-21 12:12:50 +0000

    refactor code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,13 @@
 
 void print_alphabet(void)
 {
+int count = 0;
 	char letter;
-for (int i = 0; i < 10; i++)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
-	_putchar('\n');
-}
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
