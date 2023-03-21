a63f932d577c7617c7e8b861bcdc2da9afdd85d0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-03-21 12:16:43 +0000

    refactor code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet in lowercase
+ * print_alphabet_x10 - print alphabet in lowercase
  *
  * Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int count = 0;
 	char letter;
-
-	while (count++ <= 9)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
-	}
+for (int i = 0; i < 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+}
 }
