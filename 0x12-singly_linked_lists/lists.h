610a9ed6bb2ba9c80deca160f6b9fe88a1e4eb3c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-28 15:13:53 +0000

    added a prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,7 +20,9 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* Prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
