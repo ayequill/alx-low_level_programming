47707998c758ff5dbd1ef0404cb0acf942a2c2bf
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-30 00:56:36 +0000

    returns all elements single

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_listint - prinst all the elements of a listint_t list
+* @h: pointer to the first node
+*
+* Return: number of nodes
+*/
+
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
