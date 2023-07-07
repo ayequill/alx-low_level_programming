9c3c70271edca9e0e9c882fac7b2b339313ed18c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-07 03:18:12 +0000

    refactored style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * has_table_create - creates a hash table with
+ * hash_table_create - creates a hash table with
  * a given size
  * @size: the size of the table
+ *
  * Description: Creates a hash table in memory with
- * a given size
+ * a given size and the dynamically allocates memory
+ * for the array
  *
  * Return: hash_table *
  *
@@ -13,22 +15,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
-    /* unsigned long int index = 0; */
+	hash_table_t *hash_table = NULL;
+	/* unsigned long int index = 0; */
+	if (size == 0)
+		return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
 
-    if (size == 0)
-        return (NULL);
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
 
-    hash_table = malloc(sizeof(hash_table_t));
-
-    hash_table->size = size;
-    hash_table->array = calloc(size, sizeof(hash_node_t *));
-
-    if (!hash_table->array)
-    {
-        free(hash_table);
-        return NULL;
-    }
-
-    return (hash_table);
+	if (!hash_table->array)
+	{
+		free(hash_table);
+			return (NULL);
+	}
+	return (hash_table);
 }
